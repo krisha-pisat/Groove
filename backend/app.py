5c0386edc094de3ce27c7cdc3740c82275e5862f
JxSH-07 <jashdjsce36@gmail.com>
2025-10-16 22:08:19 +0530

    Implemented Docker

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from ytmusicapi import YTMusic
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -31,4 +32,5 @@ def search():
     return jsonify({'results': songs})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))  # use Cloud Run port if available
+    app.run(host="0.0.0.0", port=port)
